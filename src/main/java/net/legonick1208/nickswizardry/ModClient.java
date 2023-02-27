9af9d80fcb08d30d361e3d7777adf740daca685c
LegoNick1208 <nshreve@outlook.com>
2023-02-26 21:05:24 -0500

    Initial commit

---
+++ src/main/java/net/legonick1208/nickswizardry/ModClient.java
@@ -0,0 +1,11 @@
+package net.legonick1208.nickswizardry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/legonick1208/nickswizardry/NicksWizardryMod.java
@@ -0,0 +1,19 @@
+package net.legonick1208.nickswizardry;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NicksWizardryMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "nickswizardry";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
